189e70ad4aa6384295941a1732a88879ff741d7c
Jay <jay.tamayo@98labs.com>
2025-07-24 20:09:30 +0800

    fix configure vercel

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
   },
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
