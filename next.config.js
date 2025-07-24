95480ccf432f7604702a15fb74578423de75fcfa
Jay <jay.tamayo@98labs.com>
2025-07-24 18:55:37 +0800

    deployment configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
